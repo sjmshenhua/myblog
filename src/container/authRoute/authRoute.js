580ceb54b2e77faf111b9a76fa8eda37e0296c5a
sjmshenhua <zw851477603@qq.com>
2018-06-07 14:33:36 +0800

    modify authRoute

--- src/container/authRoute/authRoute.js
+++ src/container/authRoute/authRoute.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import axios from 'axios';
-
+import { withRouter } from 'react-router-dom';
+@withRouter
 class AuthRoute extends React.Component{
     componentDidMount(){
-        console.log(1)
         //获取用户信息
         axios.get('/user/info')
         .then((res)=>{
-            console.log(res)
+            
+            if(res.status == 200){
+                if(res.data.code == 0){
+                    //有登录信息
+                }else{
+                    this.props.history.push('/login')
+                }
+            }else{
+                console.log(res.data)
+            }
+            
         })
     }
     render(){
