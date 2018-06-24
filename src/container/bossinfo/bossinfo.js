23c18540ff6e1ad13e74e8a3deff65672193173e
sjmshenhua <zw851477603@qq.com>
2018-06-25 01:58:23 +0800

    boss完善信息页面完成

--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { NavBar } from 'antd-mobile';
+import { NavBar, InputItem, TextareaItem, Button } from 'antd-mobile';
 import AvatarSelector from '../../component/avatar-selector/avatar-selector';
 
 class BossInfo extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    onChange(key,val){
+        console.log(key)
+        this.setState({
+            [key]: val
+        })
+    }
     render(){
         return(
             <div>
                 <NavBar mode="dark">BOSS完善信息页</NavBar>
-                <AvatarSelector></AvatarSelector>
+                {/* <AvatarSelector selectAvatar={this.selectAvatar}></AvatarSelector> */}
+                <AvatarSelector selectAvatar={(imgname)=>{
+                    this.setState({
+                        avatar: imgname
+                    })
+                }}></AvatarSelector>
+                <InputItem onChange={v=>this.onChange('title',v)} >招聘职位</InputItem>
+                <InputItem onChange={v=>this.onChange('company',v)} >公司名称</InputItem>
+                <InputItem onChange={v=>this.onChange('money',v)} >职位薪资</InputItem>
+                <TextareaItem title="职位要求" rows={3} autoHeight onChange={v=>this.onChange('desc',v)} ></TextareaItem>
+                <Button type="primary">保存</Button>
+
+
             </div>
         )
     }
